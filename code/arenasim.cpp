5bcbde41f33d55d55e373010f108aafe6ccb6a0a
Patryk <allimperfectlove@proton.me>
2025-12-18 14:38:46 +0100

    Continuing to work on buttons

--- code/arenasim.cpp
+++ code/arenasim.cpp
@@ -1,39 +1,127 @@
 #include "raylib.h"
 
-struct Warrior {
+#define internal static
+#define local_persist static
+#define global_variable static
+
+struct Warrior
+{
     Color COLOR;
     int health;
     int damage;
 };
 
-const char* DrawMenu()
+// TODO(Patryk):
+
+// void DrawButton(int posX, int posY, int width, int height, const char* text)
+// {
+//     // Button state: 0-NORMAL, 1-MOUSE_HOVER, 2-PRESSED
+//     Vector2 mousePos = GetMousePosition();
+//     int buttonState = 0;
+//     Color buttonColor;
+//     DrawRectangle(posX, posY, width, height, buttonColor);
+//     DrawRectangleLines(posX, posY, width, height, BLACK);
+//     DrawText(text, posX + 10, posY, 24, BLACK);
+
+//     Rectangle buttonBounds = {posX, posY, width, height};
+//     if (CheckCollisionPointRec(mousePos, buttonBounds))
+//     {
+// 	if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) buttonColor = BLUE;
+// 	else buttonState = 1;
+
+// 	// if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) btnAction = true;
+//     }
+//     else buttonState = 0;
+// }
+
+void DrawButton(int posX, int posY, int width, int height, const char* text, bool isSelected)
 {
-    int positionX = 50;
-    int positionX2 = 400;
-    int positionY = 100;
-    int width = 150;
-    int height = 50;
+    Vector2 mousePos = GetMousePosition();
+    Rectangle buttonBounds = {(float)posX, (float)posY, (float)width, (float)height};
+    
+    // Logic: If it's already selected, keep it blue. 
+    // Otherwise, check for hover/click.
+    Color buttonColor = LIGHTGRAY;
+    
+    if (isSelected) 
+    {
+        buttonColor = RED;
+    }
+    else if (CheckCollisionPointRec(mousePos, buttonBounds))
+    {
+        buttonColor = GRAY; // Hover color
+    }
+
+    DrawRectangleRec(buttonBounds, buttonColor);
+    DrawRectangleLines(posX, posY, width, height, BLACK);
+    DrawText(text, posX + 10, posY + 10, 20, BLACK);
+}
+
+// int selectedBall = -1;
+
+// void DrawMenu()
+// {
+ 
+//     int positionX = 50;
+//     int positionX2 = 400;
+//     int positionY = 100;
+//     int width = 150;
+//     int height = 50;
+//     int x = 50;
+//     int y = 100;
+//     int w = 150;
+//     int h = 50;
+//     const char* balls[] = {"Unarmed", "Sword", "Spear", "Duplicator", "Dagger"};
+//     Vector2 mousePos = GetMousePosition();
+//     DrawText("Chose ball no.1", positionX-10, positionY-75, 24, BLACK);
+//     for (int i = 0; i < 5; i++)
+//     {
+//         Rectangle bounds = {(float)x, (float)y, (float)w, (float)h};
+//         if (CheckCollisionPointRec(mousePos, bounds) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+//         {
+//             selectedBall = i; // Save the choice!
+//         }
+
+//         DrawButton(x, y + (i * 100), w, h, balls[i], (selectedBall == i));
+//     }
+
+//     DrawText("Chose ball no.2", positionX2, positionY-75, 24, BLACK);
+//     for (int i = 0; i < 5; i++)
+//     {
+ 
+//     }
+
+//     DrawRectangle(275, 275, height, height, YELLOW);
+//     DrawRectangleLines(275, 275, height, height, BLACK);
+// }
+
+global_variable int selectedBall = -1;
+
+internal void
+DrawMenu()
+{
     const char* balls[] = {"Unarmed", "Sword", "Spear", "Duplicator", "Dagger"};
-    DrawText("Chose ball no.1", positionX-10, positionY-75, 24, BLACK);
+    Vector2 mousePos = GetMousePosition();
+
+    DrawText("Choose ball no.1", 40, 25, 20, BLACK);
+
     for (int i = 0; i < 5; i++)
     {
-        DrawRectangle(positionX, positionY + (i * 100), width, height, LIGHTGRAY);
-	DrawRectangleLines(positionX, positionY + (i * 100), width, height, BLACK);
-	DrawText(balls[i], positionX + 10, positionY + 10 + (i * 100), 24, BLACK);       
+        int x = 50;
+        int y = 100 + (i * 100);
+        int w = 150;
+        int h = 50;
+
+        // Check if THIS specific button is the one the user clicked
+        Rectangle bounds = {(float)x, (float)y, (float)w, (float)h};
+        if (CheckCollisionPointRec(mousePos, bounds) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            selectedBall = i; // Save the choice!
+        }
+
+        // Draw the button, telling it if it should be blue (isSelected)
+        DrawButton(x, y, w, h, balls[i], (selectedBall == i));
     }
-
-    DrawText("Chose ball no.2", positionX2, positionY-75, 24, BLACK);
-    for (int i = 0; i < 5; i++)
-    {
-        DrawRectangle(positionX2, positionY + (i * 100), width, height, LIGHTGRAY);
-	DrawRectangleLines(positionX2, positionY + (i * 100), width, height, BLACK);
-	DrawText(balls[i], positionX2 + 10, positionY + 10 + (i * 100), 24, BLACK);       
-    }
-
-    DrawRectangle(275, 275, height, height, YELLOW);
-    DrawRectangleLines(275, 275, height, height, BLACK);
-
-    return balls[1];
 }
 
 void DrawBalls()
@@ -48,6 +136,7 @@ int main(void)
 
     while (!WindowShouldClose())
     {
+	
         BeginDrawing();
             ClearBackground(RAYWHITE);
 
